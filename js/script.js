2678fd52a351f8ad0f107568758eab3921f09e53
Giacomo-Arcangeli <giacomoarcangeli1997@gmail.com>
2023-06-08 21:57:10 +0200

    add milestone 3 todo fix

--- js/script.js
+++ js/script.js
@@ -1,36 +1,89 @@
 // recupero gli elementi dal DOM
 const button = document.querySelector('button');
 const squareBox = document.getElementById('square-box');
+const scoreElement = document.getElementById('score');
 
-
-// **************
+// *********************
 // FUNZIONI
-function createsquare(){
+// *********************
+
+// funzione per creare i quadrati
+function createsquares(){
     const square = document.createElement('div');
-    square.classList.add('square' , 'd-none');
+    square.classList.add('square');
+    squareBox.classList.add('d-none');
 
     button.addEventListener('click' , function(){
-        square.classList.remove('d-none');
+        squareBox.classList.remove('d-none');
     })
 
     return square;
 }
-// **************
 
-// creo i quadrati
+// funzione per creare le bombe
+function createBombs (numberOfBombs , squaresNumber){
+    let bombs = [];
+    
+    while(bombs.length < numberOfBombs){
+        let randomNumber;
+        do {
+            randomNumber = Math.floor(Math.random() * squaresNumber) + 1;
+        } while (bombs.includes(randomNumber));
+        bombs.push(randomNumber);
+    }
+    return bombs;
+}
+
+// *********************
+
+// disegno i quadrati
 const rows = 10;
 const cols = 10;
 const numberOfSquares = rows * cols;
 
-// al click dei quadrati...
-for (let i = 0; i < numberOfSquares; i++){
-    const square  = createsquare();
-    square.innerText= i + 1;
+// punteggio
+let score = 0;
 
+// numero bombe
+const totalBombs = 16;
+
+// AL PLAY...
+button.addEventListener('click' , function(){
+
+    // lista delle bombe
+    const bombs = [createBombs(totalBombs , numberOfSquares)];
+    console.log (bombs);
+})
+
+
+// ? ********* LOGICA **********
+
+// al click dei quadrati...
+for (let i = 1; i <= numberOfSquares; i++){
+    const square  = createsquares();
+    square.innerText= i;
+    
     square.addEventListener('click' , function(){
-        square.classList.toggle('clicked');
-        console.log(i + 1);
+        
+        if(square.classList.contains('clicked')) return;
+        
+        square.classList.add('clicked');
+        console.log(i);
+        
+        const isBomb = bombs.includes(i);
+
+    if (isBomb){
+        square.classList.add('bomb');
+        console.log('Hai perso! Hai realizzato:' + ' ' + score + ' ' + 'punti');
+    }else {
+        scoreElement.innerText = ('Punti:' + ' ' + ++score);
+
+    }
+
     })
 
     squareBox.appendChild(square);
+
 }
+
+
